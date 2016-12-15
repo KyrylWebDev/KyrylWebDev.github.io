113eaf4ddf7fcc91ae4414619c63badcc0648d0f
Kyryl Ivashyn <kyrylivashyn@gmail.com>
2016-12-16 01:36:59 +0200

    made mobile first

---
+++ FINAM-EXAM/gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const debug = require('gulp-debug');
+const uglify = require('gulp-uglifyjs');
+const watch = require('gulp-watch');
+
+
+gulp.task('sass', function() {
+    return gulp.src('css/*.sass')
+        .pipe(debug({title: 'src'}))
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('css/build'))
+        .pipe(debug({title: 'dest'}));
+})
+
+gulp.task('js', function() {
+    return gulp.src('js/*.js')
+        .pipe(debug({ title: 'src' }))
+        .pipe(uglify('production.min.js'))
+        .pipe(gulp.dest('js/build'))
+        .pipe(debug({ title: 'dest' }))
+})
+
+gulp.task('watch', function() {
+    gulp.watch('css/style.sass', ['sass']);
+});
